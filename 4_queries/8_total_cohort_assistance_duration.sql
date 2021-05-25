7b296809bc40ad4f11676d3b3ea86c6d9e08d651
Jesse <jesse.a.lapointe@gmail.com>
2021-05-25 19:29:39 +0000

    Add final round of SELECT queries

---
+++ 4_queries/8_total_cohort_assistance_duration.sql
@@ -0,0 +1,7 @@
+SELECT cohorts.name as cohort, 
+sum(completed_at - started_at) as total_duration
+FROM cohorts
+JOIN students ON students.cohort_id = cohorts.id
+JOIN assistance_requests ON students.id = assistance_requests.student_id
+GROUP BY cohorts.name
+ORDER BY total_duration;
